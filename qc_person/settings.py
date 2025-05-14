8db57b0672d892ac91e0e28753c8d6b48fbd2319
Crescent Admin <crescent601@gmail.com>
2025-05-14 12:15:42 +0530

    Moved static folder to project root

--- qc_person/settings.py
+++ qc_person/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # This tells Django where to collect static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'replacement_app', 'static'),
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
